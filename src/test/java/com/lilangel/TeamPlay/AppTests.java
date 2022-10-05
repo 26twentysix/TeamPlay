96f96e3517cebddd5573913132e024f423dbed11
Sergey Kutas <cvndyperov@gmail.com>
2022-10-05 15:40:34 +0500

    created tests for App.testMethod

---
+++ src/test/java/com/lilangel/TeamPlay/AppTests.java
@@ -0,0 +1,17 @@
+package com.lilangel.TeamPlay;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTests {
+
+    @Test
+    public void testMethodFailInit() {
+        assertFalse(App.testMethod(false));
+    }
+
+    @Test
+    public void testMethodCorrectInit() {
+        assertTrue(App.testMethod(true));
+    }
+}
