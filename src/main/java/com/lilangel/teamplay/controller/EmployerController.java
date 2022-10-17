a3f4d788800b8efcdf9f90ceb61a70b314145bc7
Sergey Kutas <cvndyperov@gmail.com>
2022-10-17 10:53:59 +0500

    created docs

--- src/main/java/com/lilangel/teamplay/controller/EmployerController.java
+++ src/main/java/com/lilangel/teamplay/controller/EmployerController.java
@@ -5,7 +5,6 @@ import com.lilangel.teamplay.service.EmployerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,9 +13,21 @@ import java.util.List;
 @RequestMapping("/employers")
 public class EmployerController {
 
+    /**
+     * Параметр запроса "id"
+     */
     private static final String ID = "id";
+    /**
+     * Параметр запроса "name"
+     */
     private static final String NAME = "name";
+    /**
+     * Параметр запроса "email"
+     */
     private static final String EMAIL = "email";
+    /**
+     * Параметр запроса "team_id"
+     */
     private static final String TEAM_ID = "team_id";
 
     private final EmployerService employerService;
@@ -26,12 +37,23 @@ public class EmployerController {
         this.employerService = employerService;
     }
 
+    /**
+     * Возвращает информацию о сотруднике по id
+     *
+     * @param id идентификатор сотрудника
+     * @return ответ с информацией о сотруднике и HTTP-статусом 200
+     */
     @GetMapping(value = "/get?{id}")
     public ResponseEntity<Employer> getById(@PathVariable Integer id) {
         Employer employer = employerService.getById();
         return new ResponseEntity<>(employer, HttpStatus.OK);
     }
 
+    /**
+     * Возвращает информацию о всех сотрудниках
+     *
+     * @return ответ с информацией о всех сотрудниках и HTTP-статусом 200
+     */
     @GetMapping(value = "/getall")
     public ResponseEntity<List<Employer>> getAll() {
         List<Employer> allEmployers = employerService.getAll();
@@ -39,15 +61,29 @@ public class EmployerController {
 
     }
 
+    /**
+     * Создает нового сотрудника
+     *
+     * @param name   Имя Фамилия сотрудника
+     * @param email  Адрес электронной почты
+     * @param teamId Идентификатор команды
+     * @return ответ с информацией о всех сотрудниках и HTTP-статусом 201
+     */
     @PostMapping(value = "/create")
-    public ResponseEntity<String> create(@RequestParam(ID) Integer id,
-                                    @RequestParam(NAME) String name,
-                                    @RequestParam(EMAIL) String email,
-                                    @RequestParam(TEAM_ID) Integer teamId) {
-        Integer createdId = employerService.createNewEmployer(id, name, email, teamId);
+    public ResponseEntity<String> create(
+            @RequestParam(NAME) String name,
+            @RequestParam(EMAIL) String email,
+            @RequestParam(TEAM_ID) Integer teamId) {
+        Integer createdId = employerService.createNewEmployer(name, email, teamId);
         return new ResponseEntity<>(createdId.toString(), HttpStatus.CREATED);
     }
 
+    /**
+     * Удаляет сотрудника по его идентификатору
+     *
+     * @param id идентификатор сотрудника
+     * @return ответ с HTTP-статусом 200
+     */
     @DeleteMapping(value = "/delete?{id}")
     public ResponseEntity<?> deleteById(@PathVariable Integer id) {
         employerService.deleteById(id);
