63508fe27248802f44893f601203f3e9b80fa746
Saveliy21 <vildanov.s@bk.ru>
2022-10-19 16:48:25 +0500

    Dao done

--- src/main/java/com/lilangel/teamplay/dao/impl/EmployerDaoImpl.java
+++ src/main/java/com/lilangel/teamplay/dao/impl/EmployerDaoImpl.java
@@ -1,7 +1,40 @@
 package com.lilangel.teamplay.dao.impl;
 
 import com.lilangel.teamplay.dao.EmployerDao;
+import com.lilangel.teamplay.models.Employer;
+import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+
+@Service
 public class EmployerDaoImpl implements EmployerDao {
-    //TODO
+
+    private final HashMap<Integer, Employer> employers;
+    private static Integer currentId;
+
+    public EmployerDaoImpl() {
+        this.employers = new HashMap<>();
+        currentId = 0;
+    }
+
+    @Override
+    public HashMap<Integer, Employer> getAllEmployers() {
+        return employers;
+    }
+
+    @Override
+    public void delete(Integer id) {
+        employers.remove(id);
+    }
+
+    @Override
+    public void add(Employer employer) {
+        employer.setId(currentId++);
+        employers.put(employer.getId(), employer);
+    }
+
+    @Override
+    public Employer getById(Integer id) {
+        return employers.get(id);
+    }
 }
--- src/main/java/com/lilangel/teamplay/models/Employer.java
+++ src/main/java/com/lilangel/teamplay/models/Employer.java
@@ -1,5 +1,18 @@
 package com.lilangel.teamplay.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Employer {
-    //TODO
+
+    private Integer id;
+
+    private String email;
+
+    private String name;
+
+    private Integer teamId;
+
 }
