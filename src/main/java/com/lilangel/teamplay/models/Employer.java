49260dce50c9c9bf03584dd096f69ab50bc66d85
Margarita Yarmolchuk <margaritayarmolchuk@gmail.com>
2022-10-20 15:52:15 +0500

    UPD employer class.

--- src/main/java/com/lilangel/teamplay/models/Employer.java
+++ src/main/java/com/lilangel/teamplay/models/Employer.java
@@ -3,10 +3,15 @@ package com.lilangel.teamplay.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.criteria.CriteriaBuilder;
+
 @Data
 @AllArgsConstructor
 public class Employer {
-
+    @Id
+    @GeneratedValue
     private Integer id;
 
     private String email;
@@ -14,5 +19,14 @@ public class Employer {
     private String name;
 
     private Integer teamId;
+
+    public Employer(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
 
 }
