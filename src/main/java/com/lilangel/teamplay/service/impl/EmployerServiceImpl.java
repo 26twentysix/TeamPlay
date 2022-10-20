755b9dc8a95c2beac7f22ae89ec629db31aa8077
Margarita Yarmolchuk <margaritayarmolchuk@gmail.com>
2022-10-20 16:03:52 +0500

    Added impl for service.

--- src/main/java/com/lilangel/teamplay/service/impl/EmployerServiceImpl.java
+++ src/main/java/com/lilangel/teamplay/service/impl/EmployerServiceImpl.java
@@ -1,7 +1,44 @@
 package com.lilangel.teamplay.service.impl;
 
+import com.lilangel.teamplay.dao.EmployerDao;
+import com.lilangel.teamplay.exception.EmployerNotFoundException;
+import com.lilangel.teamplay.models.Employer;
 import com.lilangel.teamplay.service.EmployerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.HashMap;
+
+@Service
 public class EmployerServiceImpl implements EmployerService {
-    //TODO
+    private final EmployerDao employerDao;
+
+    @Autowired
+    public EmployerServiceImpl(EmployerDao employerDao) {
+        this.employerDao = employerDao;
+    }
+
+    @Override
+    public Integer createNewEmployer(String name, String email, Integer teamId) throws IOException {
+        Employer employer = new Employer(name, email);
+        Integer id = employer.getId();
+        employerDao.add(employer);
+        return id;
+    }
+
+    @Override
+    public void deleteById(Integer id) throws EmployerNotFoundException, IOException {
+        employerDao.delete(id);
+    }
+
+    @Override
+    public Employer getById(Integer id) throws EmployerNotFoundException, IOException {
+        return employerDao.getById(id);
+    }
+
+    @Override
+    public HashMap<Integer, Employer> getAll() throws IOException {
+        return employerDao.getAllEmployers();
+    }
 }
