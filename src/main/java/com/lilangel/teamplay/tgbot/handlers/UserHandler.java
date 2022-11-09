9e8f12ad3f71e8c8c239a69b784aaacad7945acb
Sergey Kutas <cvndyperov@gmail.com>
2022-11-10 01:36:18 +0500

    created empty handler for "/user" command

---
+++ src/main/java/com/lilangel/teamplay/tgbot/handlers/UserHandler.java
@@ -0,0 +1,11 @@
+package com.lilangel.teamplay.tgbot.handlers;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserHandler extends AbstractHandler{
+    @Override
+    public String requestHandler(String message) {
+        return null;
+    }
+}
