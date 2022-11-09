cbd79fdff48973638e4bf64fa719881ca31a4f27
Sergey Kutas <cvndyperov@gmail.com>
2022-11-10 01:35:07 +0500

    added some exceptions

---
+++ src/main/java/com/lilangel/teamplay/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.lilangel.teamplay.exception;
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
