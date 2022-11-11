8863e98b6ba2e1417946f7d20e1cff2d4f908acd
Sergey Kutas <cvndyperov@gmail.com>
2022-11-11 18:23:48 +0500

    add tests

--- src/test/java/com/lilangel/teamplay/tgbot/handlers/EmployerHandlerTest.java
+++ src/test/java/com/lilangel/teamplay/tgbot/handlers/EmployerHandlerTest.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -16,7 +19,7 @@ class EmployerHandlerTest {
     /**
      * Запрос на создание сотрудника
      */
-    private final String CREATE_REQUEST = "/employers create John Doe johndoe@test.com 1";
+    private final String CREATE_REQUEST = "/employers create name=John Doe email=johndoe@test.com team_id=1";
 
     private final EmployerHandler employerHandler;
 
@@ -26,102 +29,116 @@ class EmployerHandlerTest {
     }
 
     /**
-     * Тестирует метод {@link EmployerHandler#requestHandler(String)} с запросом, содержащим команду "help"
+     * Тестирует метод {@link EmployerHandler#requestHandler(String, java.util.Map)}
+     * с запросом, содержащим команду "help"
      * Метод проходит проверку, если возрвращается ответ, начинающийся с "/employer Help:"
      */
     @Test
     public void getHelpMessageTest() {
         String request = "/employers help";
-        String response = employerHandler.requestHandler(request);
+        String response = employerHandler.requestHandler(request, new HashMap<>());
         assertTrue(response.startsWith("/employer Help:"));
     }
 
     /**
-     * Тестирует метод {@link EmployerHandler#requestHandler(String)} с запросом, содержащим команду "wrongCommand"
+     * Тестирует метод {@link EmployerHandler#requestHandler(String, java.util.Map)}
+     * с запросом, содержащим команду "wrongCommand"
      * Метод проходит проверку, если возрвращается ответ, начинающийся с "Wrong command"
      */
     @Test
     public void wrongCommandTest() {
         String request = "/employers wrongCommand";
-        String response = employerHandler.requestHandler(request);
+        String response = employerHandler.requestHandler(request, new HashMap<>());
         assertTrue(response.startsWith("Wrong command"));
     }
 
     /**
-     * Тестирует метод {@link EmployerHandler#requestHandler(String)} с запросом, содержащим команду "getAll"
+     * Тестирует метод {@link EmployerHandler#requestHandler(String, java.util.Map)}
+     * с запросом, содержащим команду "getAll"
      * Метод проходит проверку, если возрвращается непустой ответ, начинающийся с "Employers:"
      */
     @Test
     public void getAllTest() {
         String request = "/employers getAll";
-        employerHandler.requestHandler(CREATE_REQUEST);
-        String response = employerHandler.requestHandler(request);
+        employerHandler.requestHandler(CREATE_REQUEST, new HashMap<>());
+        String response = employerHandler.requestHandler(request, new HashMap<>());
         assertNotNull(response);
         assertTrue(response.startsWith("Employers:"));
     }
 
     /**
-     * Тестирует метод {@link EmployerHandler#requestHandler(String)} с запросом, содержащим команду "getById"
+     * Тестирует метод {@link EmployerHandler#requestHandler(String, java.util.Map)}
+     * с запросом, содержащим команду "getById"
      * Метод проходит проверку, если возрвращается непустой ответ, начинающийся с "Employer:"
      */
     @Test
     public void getByIdTest() {
-        String createdId = employerHandler.requestHandler(CREATE_REQUEST);
+        String createdId = employerHandler.requestHandler(CREATE_REQUEST, new HashMap<>());
         var parsedResp = createdId.split(" ");
         String request = "/employers getById " + parsedResp[parsedResp.length - 1];
-        String response = employerHandler.requestHandler(request);
+        Map<String, String> args = new HashMap<>();
+        args.put("id", parsedResp[parsedResp.length - 1]);
+        String response = employerHandler.requestHandler(request, args);
         assertNotNull(response);
         assertTrue(response.startsWith("Employer:"));
     }
 
     /**
-     * Тестирует метод {@link EmployerHandler#requestHandler(String)}
-     * с запросом, содержащим команду "deleteById {previouslyCreatedId}"
+     * Тестирует метод {@link EmployerHandler#requestHandler(String, java.util.Map)}
+     * с запросом, содержащим команду "deleteById id={previouslyCreatedId}"
      * Метод проходит проверку, если возрвращается непустой ответ, начинающийся с "Successfully"
      */
     @Test
     public void deleteByIdTest() {
-        String createdId = employerHandler.requestHandler(CREATE_REQUEST);
+        String createdId = employerHandler.requestHandler(CREATE_REQUEST, new HashMap<>());
         var parsedResp = createdId.split(" ");
         String request = "/employers deleteById " + parsedResp[parsedResp.length - 1];
-        String response = employerHandler.requestHandler(request);
+        Map<String, String> args = new HashMap<>();
+        args.put("id", parsedResp[parsedResp.length - 1]);
+        String response = employerHandler.requestHandler(request, args);
         assertNotNull(response);
         assertTrue(response.startsWith("Successfully"));
     }
 
     /**
-     * Тестирует метод {@link EmployerHandler#requestHandler(String)}
-     * с запросом, содержащим команду "deleteById -1"
-     * Метод проходит проверку, если возрвращается непустой ответ, начинающийся с "Successfully"
+     * Тестирует метод {@link EmployerHandler#requestHandler(String, java.util.Map)}
+     * с запросом, содержащим команду "deleteById id=-1"
+     * Метод проходит проверку, если возвращается непустой ответ, начинающийся с "Employer with given ID"
      */
     @Test
     public void deleteByWrongIdTest() {
-        String request = "/employers deleteById " + "-1";
-        String response = employerHandler.requestHandler(request);
+        String request = "/employers deleteById -1";
+        Map<String, String> args = new HashMap<>();
+        args.put("id", "-1");
+        String response = employerHandler.requestHandler(request, args);
         assertNotNull(response);
         assertTrue(response.startsWith("Employer with given ID"));
     }
 
     /**
-     * Тестирует метод {@link EmployerHandler#requestHandler(String)}
-     * с запросом, содержащим команду "create John Doe johndoe@test.com 1"
+     * Тестирует метод {@link EmployerHandler#requestHandler(String, java.util.Map)}
+     * с запросом, содержащим команду "create name=John Doe email=johndoe@test.com team_id=1"
      * Метод проходит проверку, если возрвращается непустой ответ, начинающийся с "Successfully"
      */
     @Test
     public void createTest() {
-        String response = employerHandler.requestHandler(CREATE_REQUEST);
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "John Doe");
+        args.put("email", "johndoe@test.com");
+        args.put("team_id", "1");
+        String response = employerHandler.requestHandler(CREATE_REQUEST, args);
         assertNotNull(response);
         assertTrue(response.startsWith("Successfully"));
     }
 
     /**
-     * Тестирует метод {@link EmployerHandler#requestHandler(String)}
-     * с запросом, содержащим команду "create Doe johndoe@test.com 1"
+     * Тестирует метод {@link EmployerHandler#requestHandler(String, java.util.Map)}
+     * с запросом, содержащим команду "create"
      * Метод проходит проверку, если возрвращается непустой ответ, начинающийся с "Wrong args"
      */
     @Test
     public void createWithWrongArgsNum() {
-        String response = employerHandler.requestHandler("/employers create Doe johndoe@test.com 1");
+        String response = employerHandler.requestHandler("/employers create", new HashMap<>());
         assertNotNull(response);
         System.out.println(response);
         assertTrue(response.startsWith("Wrong args"));
---
+++ src/test/java/com/lilangel/teamplay/tgbot/handlers/TeamHandlerTest.java
@@ -0,0 +1,146 @@
+package com.lilangel.teamplay.tgbot.handlers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Класс юнит-тестов для {@link EmployerHandler}
+ */
+@SpringBootTest
+@Transactional
+class TeamHandlerTest {
+    /**
+     * Запрос на создание проекта
+     */
+    private final String CREATE_REQUEST = "/team create name=NewTeam lead_id=2";
+
+    private final TeamHandler teamHandler;
+
+    @Autowired
+    public TeamHandlerTest(TeamHandler teamHandler) {
+        this.teamHandler = teamHandler;
+    }
+
+    /**
+     * Тестирует метод {@link TeamHandler#requestHandler(String, java.util.Map)}
+     * с запросом, содержащим команду "help"
+     * Метод проходит проверку, если возрвращается ответ, начинающийся с "/team Help:"
+     */
+    @Test
+    public void getHelpMessageTest() {
+        String request = "/team help";
+        String response = teamHandler.requestHandler(request, new HashMap<>());
+        assertTrue(response.startsWith("/team Help:"));
+    }
+
+    /**
+     * Тестирует метод {@link TeamHandler#requestHandler(String, java.util.Map)}
+     * с запросом, содержащим команду "wrongCommand"
+     * Метод проходит проверку, если возрвращается ответ, начинающийся с "Wrong command"
+     */
+    @Test
+    public void wrongCommandTest() {
+        String request = "/team wrongCommand";
+        String response = teamHandler.requestHandler(request, new HashMap<>());
+        assertTrue(response.startsWith("Wrong command"));
+    }
+
+    /**
+     * Тестирует метод {@link TeamHandler#requestHandler(String, java.util.Map)}
+     * с запросом, содержащим команду "getAll"
+     * Метод проходит проверку, если возрвращается непустой ответ, начинающийся с "Teams:"
+     */
+    @Test
+    public void getAllTest() {
+        String request = "/team getAll";
+        teamHandler.requestHandler(CREATE_REQUEST, new HashMap<>());
+        String response = teamHandler.requestHandler(request, new HashMap<>());
+        assertNotNull(response);
+        assertTrue(response.startsWith("Teams:"));
+    }
+
+    /**
+     * Тестирует метод {@link TeamHandler#requestHandler(String, java.util.Map)}
+     * с запросом, содержащим команду "getById"
+     * Метод проходит проверку, если возрвращается непустой ответ, начинающийся с "Team:"
+     */
+    @Test
+    public void getByIdTest() {
+        String createdId = teamHandler.requestHandler(CREATE_REQUEST, new HashMap<>());
+        var parsedResp = createdId.split(" ");
+        String request = "/team getById id=" + parsedResp[parsedResp.length - 1];
+        Map<String, String> args = new HashMap<>();
+        args.put("id", parsedResp[parsedResp.length - 1]);
+        String response = teamHandler.requestHandler(request, args);
+        assertNotNull(response);
+        assertTrue(response.startsWith("Team:"));
+    }
+
+    /**
+     * Тестирует метод {@link TeamHandler#requestHandler(String, java.util.Map)}
+     * с запросом, содержащим команду "deleteById id={previouslyCreatedId}"
+     * Метод проходит проверку, если возрвращается непустой ответ, начинающийся с "Successfully"
+     */
+    @Test
+    public void deleteByIdTest() {
+        String createdId = teamHandler.requestHandler(CREATE_REQUEST, new HashMap<>());
+        var parsedResp = createdId.split(" ");
+        String request = "/team deleteById id=" + parsedResp[parsedResp.length - 1];
+        Map<String, String> args = new HashMap<>();
+        args.put("id", parsedResp[parsedResp.length - 1]);
+        String response = teamHandler.requestHandler(request, args);
+        assertNotNull(response);
+        assertTrue(response.startsWith("Successfully"));
+    }
+
+    /**
+     * Тестирует метод {@link TeamHandler#requestHandler(String, java.util.Map)}
+     * с запросом, содержащим команду "deleteById id=-1"
+     * Метод проходит проверку, если возвращается непустой ответ, начинающийся с "Team with given ID"
+     */
+    @Test
+    public void deleteByWrongIdTest() {
+        String request = "/team deleteById id=-1";
+        Map<String, String> args = new HashMap<>();
+        args.put("id", "-1");
+        String response = teamHandler.requestHandler(request, args);
+        assertNotNull(response);
+        assertTrue(response.startsWith("Team with given ID"));
+    }
+
+    /**
+     * Тестирует метод {@link TeamHandler#requestHandler(String, java.util.Map)}
+     * с запросом, содержащим команду "create name=NewTeam lead_id=2"
+     * Метод проходит проверку, если возрвращается непустой ответ, начинающийся с "Successfully"
+     */
+    @Test
+    public void createTest() {
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "NewTeam");
+        args.put("lead_id", "2");
+        String response = teamHandler.requestHandler(CREATE_REQUEST, args);
+        assertNotNull(response);
+        assertTrue(response.startsWith("Successfully"));
+    }
+
+    /**
+     * Тестирует метод {@link TeamHandler#requestHandler(String, java.util.Map)}
+     * с запросом, содержащим команду "create"
+     * Метод проходит проверку, если возрвращается непустой ответ, начинающийся с "Wrong args"
+     */
+    @Test
+    public void createWithWrongArgsNum() {
+        String response = teamHandler.requestHandler("/team create", new HashMap<>());
+        assertNotNull(response);
+        System.out.println(response);
+        assertTrue(response.startsWith("Wrong args"));
+    }
+
+}
