51941861ed36b5e10bf79b78333370a19590faf0
Margarita <margaritayarmolchuk@gmail.com>
2022-11-22 17:09:32 +0500

    Added ProjectServiceImpl and ProjectRepository.

--- src/main/java/com/lilangel/teamplay/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/lilangel/teamplay/service/impl/ProjectServiceImpl.java
@@ -1,4 +1,56 @@
 package com.lilangel.teamplay.service.impl;
 
-public class ProjectServiceImpl {
+import com.lilangel.teamplay.exception.ProjectNotFoundException;
+import com.lilangel.teamplay.models.Project;
+import com.lilangel.teamplay.repository.ProjectRepository;
+import com.lilangel.teamplay.service.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProjectServiceImpl implements ProjectService {
+    private final ProjectRepository projectRepository;
+
+    @Autowired
+    public ProjectServiceImpl(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    @Override
+    public Integer saveNewProject(String name, String description, Integer teamId) {
+        Project project = new Project(name, description, teamId);
+        projectRepository.save(project);
+        return project.getId();
+    }
+
+    @Override
+    public void deleteById(Integer id) throws ProjectNotFoundException {
+        try {
+            projectRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ProjectNotFoundException("Employer with given ID was not found.");
+        }
+    }
+
+    @Override
+    public Project getById(Integer id) throws ProjectNotFoundException {
+        Optional<Project> project = projectRepository.findById(id);
+        if (project.isPresent()) {
+            return project.get();
+        } else {
+            throw new ProjectNotFoundException("Employer with given ID was not found.");
+        }
+    }
+
+    @Override
+    public List<Project> getAll() {
+        List<Project> projects = new ArrayList<>();
+        projectRepository.findAll().forEach(projects::add);
+        return projects;
+    }
 }
