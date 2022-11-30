d66e15417e030c9e02180d9647764fd583029a74
lilangel <cvndyperov@gmail.com>
2022-11-30 22:12:05 +0500

    upd AbstractHandler

--- src/main/java/com/lilangel/teamplay/tgbot/handlers/AbstractHandler.java
+++ src/main/java/com/lilangel/teamplay/tgbot/handlers/AbstractHandler.java
@@ -1,7 +1,25 @@
 package com.lilangel.teamplay.tgbot.handlers;
 
+import com.lilangel.teamplay.exception.EmployerNotFoundException;
+import com.lilangel.teamplay.models.Employer;
+
+import java.util.List;
 import java.util.Map;
 
 public abstract class AbstractHandler {
     public abstract String requestHandler(String message, Map<String, String> args);
+
+    protected abstract String getHelpMessage();
+
+    protected abstract String getWrongCommandMessage();
+
+    protected abstract String help(Map<String, String> args);
+
+    protected abstract String getAll(Map<String, String> args);
+
+    protected abstract String getById(Map<String, String> args);
+
+    protected abstract String create(Map<String, String> args);
+
+    protected abstract String deleteById(Map<String, String> args);
 }
