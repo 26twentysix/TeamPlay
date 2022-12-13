17d319916aebbb7f1bb5e04721eccc8e144c4d4f
Saveliy21 <vildanov.s@bk.ru>
2022-12-13 14:25:22 +0500

    UserServiceImpl.java changes

--- src/main/java/com/lilangel/teamplay/service/impl/UserServiceImpl.java
+++ src/main/java/com/lilangel/teamplay/service/impl/UserServiceImpl.java
@@ -1,8 +1,12 @@
 package com.lilangel.teamplay.service.impl;
 
+import com.lilangel.teamplay.exception.TicketNotFoundException;
 import com.lilangel.teamplay.exception.UserNotFoundException;
+import com.lilangel.teamplay.models.Ticket;
 import com.lilangel.teamplay.models.User;
+import com.lilangel.teamplay.repository.TicketRepository;
 import com.lilangel.teamplay.repository.UserRepository;
+import com.lilangel.teamplay.service.DefaultUserService;
 import com.lilangel.teamplay.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -13,12 +17,14 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, DefaultUserService {
     private final UserRepository userRepository;
+    private final TicketRepository ticketRepository;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, TicketRepository ticketRepository) {
         this.userRepository = userRepository;
+        this.ticketRepository = ticketRepository;
     }
 
     @Override
@@ -60,5 +66,42 @@ public class UserServiceImpl implements UserService {
 
     public User getByTgId(Long tgId) {
         return userRepository.getUserByTgId(tgId);
+    }
+
+
+    @Override
+    public void changeInfo(Integer ticketId, String... newInfo) throws TicketNotFoundException {
+        Optional<Ticket> ticket = ticketRepository.findById(ticketId);
+        if (ticket.isPresent()) {
+            Ticket chosenTicket = ticket.get();
+            if (newInfo.length == 2) {
+                chosenTicket.setPriority(newInfo[1]);
+            }
+            chosenTicket.setFullDescription(newInfo[0]);
+        } else {
+            throw new TicketNotFoundException();
+        }
+    }
+
+    @Override
+    public void pickUpTicket(Integer ticketId, Long tgId) throws UserNotFoundException {
+        Optional<Ticket> ticket = ticketRepository.findById(ticketId);
+        if (ticket.isPresent()) {
+            Ticket chosenTicket = ticket.get();
+            chosenTicket.setEmployerId(getByTgId(tgId).getEmployerId());
+        } else {
+            throw new UserNotFoundException();
+        }
+    }
+
+    @Override
+    public void changeTicketState(Integer ticketId, String status) throws TicketNotFoundException {
+        Optional<Ticket> ticket = ticketRepository.findById(ticketId);
+        if (ticket.isPresent()) {
+            Ticket chosenTicket = ticket.get();
+            chosenTicket.setStatus(status);
+        } else {
+            throw new TicketNotFoundException();
+        }
     }
 }
