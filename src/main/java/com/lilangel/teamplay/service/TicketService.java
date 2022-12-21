92a7d1411cadc3e6be416cb68343d245add7726f
Saveliy21 <vildanov.s@bk.ru>
2022-12-21 17:58:43 +0500

    TicketService updates

--- src/main/java/com/lilangel/teamplay/service/TicketService.java
+++ src/main/java/com/lilangel/teamplay/service/TicketService.java
@@ -4,6 +4,7 @@ import com.lilangel.teamplay.exception.TicketNotFoundException;
 import com.lilangel.teamplay.models.Ticket;
 
 import java.util.List;
+import java.util.Map;
 
 public interface TicketService {
     /**
@@ -43,7 +44,7 @@ public interface TicketService {
      */
     List<Ticket> getAll();
 
-    void changeTicketState(Integer ticketId, String status) throws TicketNotFoundException;
+    void updateTicketInfo(Integer ticketId, Map<String, String> args) throws TicketNotFoundException;
 
 }
 
