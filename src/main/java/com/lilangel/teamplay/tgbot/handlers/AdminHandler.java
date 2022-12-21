f9d6dc862e67727afa1f031d115da02a078df48b
lilangel <cvndyperov@gmail.com>
2022-12-21 21:51:06 +0500

    upd bot parsing message logic

--- src/main/java/com/lilangel/teamplay/tgbot/handlers/AdminHandler.java
+++ src/main/java/com/lilangel/teamplay/tgbot/handlers/AdminHandler.java
@@ -4,9 +4,7 @@ import com.lilangel.teamplay.tgbot.handlers.admins.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 @Component
 public class AdminHandler extends AbstractHandler {
@@ -17,6 +15,8 @@ public class AdminHandler extends AbstractHandler {
 
     private final Map<String, AbstractHandler> handlers = new HashMap<>();
 
+    private final Set<String> entities = new HashSet<>();
+
     @Autowired
     public AdminHandler(EmployerHandler employerHandler, ProjectHandler projectHandler, TeamHandler teamHandler,
                         TicketHandler ticketHandler, UserHandler userHandler, DefaultUserHandler defaultUserHandler) {
@@ -26,6 +26,11 @@ public class AdminHandler extends AbstractHandler {
         handlers.put("ticketHandler", ticketHandler);
         handlers.put("userHandler", userHandler);
         handlers.put("defaultUserHandler", defaultUserHandler);
+        entities.add("user");
+        entities.add("team");
+        entities.add("ticket");
+        entities.add("project");
+        entities.add("employer");
         HELP_MESSAGE = "Admin help message";
         WRONG_COMMAND_MESSAGE = "Admin wrong command message";
     }
@@ -37,20 +42,14 @@ public class AdminHandler extends AbstractHandler {
         }
         String command;
         if (message.startsWith("/")) {
-            int indexOfSpace = message.indexOf(" ");
-            if (indexOfSpace == -1) {
-                command = message;
-            } else {
-                var parsed = message.split(" ");
-                command = parsed[0];
-            }
+            command = extractFirstWord(message);
             AbstractHandler handler;
-            if (handlers.containsKey(command.substring(1) + "Handler")) {
-                handler = handlers.get(command.substring(1) + "Handler");
+            if (entities.contains(command)) {
+                handler = handlers.get(command + "Handler");
                 return handler.requestHandler(getSubCommand(message), args);
             } else {
                 handler = handlers.get("defaultUserHandler");
-                return handler.requestHandler(command.substring(1), args);
+                return handler.requestHandler(command, args);
             }
         }
         return WRONG_COMMAND_MESSAGE;
@@ -59,7 +58,7 @@ public class AdminHandler extends AbstractHandler {
     private String getSubCommand(String message) {
         var splittedString = message.split(" ");
         if (splittedString.length > 1) {
-            return splittedString[2];
+            return splittedString[1];
         }
         return "help";
     }
@@ -78,4 +77,14 @@ public class AdminHandler extends AbstractHandler {
     protected String help(Map<String, String> args) {
         return getHelpMessage();
     }
+
+    private String extractFirstWord(String message) {
+        int indexOfSpace = message.indexOf(" ");
+        if (indexOfSpace == -1) {
+            return message.substring(1);
+        } else {
+            return message.substring(1, indexOfSpace);
+        }
+    }
+
 }
